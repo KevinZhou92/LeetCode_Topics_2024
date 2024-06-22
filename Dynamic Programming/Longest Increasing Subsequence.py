a5b013ad5bda083c11b0755b9d72c436f9d7e8bb
Pengcheng Zhou <monsterzpc@gmail.com>
2024-06-21 23:40:04 -0700

    DFS + Memorization

--- Dynamic Programming/Longest Increasing Subsequence.py
+++ Dynamic Programming/Longest Increasing Subsequence.py
@@ -29,3 +29,55 @@ class Solution:
                     dp[i] = max(dp[i], dp[j] + 1)
 
         return max(dp)
+
+
+"""
+Solution 2:
+
+DFS + Memorization
+
+Time Complexity: O(n ^ 2)
+Space complexity : O(n)
+"""
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # for each num, i compare the result from a 
+        # smaller problem, for example, 
+        # 0 1 0 4 2 3  
+        # if i want to know the lis at 3, i just need to know what's the LIS
+        # at 2 and what is the LIS at 0 and check from what sequence i can get the longest sequence
+        if not nums:
+            return 0
+
+        max_lis = 1
+        self.mem = {}
+        for i in range(len(nums)):
+            for j in range(0, i):
+                if nums[j] < nums[i]:
+                    max_lis = max(max_lis, 1 + self.search(nums, j))
+        
+        return max_lis
+
+    def search(self, nums, idx):
+        if idx in self.mem:
+            return self.mem[idx]
+
+        if idx == len(nums):
+            return 0
+
+        max_lis = 1
+        for i in range(0, idx + 1):
+            if nums[i] < nums[idx]:
+                max_lis = max(max_lis, 1 + self.search(nums, i))
+        
+        self.mem[idx] = max_lis
+        
+        return max_lis
+                    
+        
+
+        
+                    
+        
+
+        
