ef92d043591018aa99255ec6b906b438b61d18bd
Pengcheng Zhou <monsterzpc@gmail.com>
2024-07-08 00:16:18 -0700

    Update Sudku solver

--- Backtracking/Sudoku Solver.py
+++ Backtracking/Sudoku Solver.py
@@ -47,3 +47,70 @@ class Solution:
                 return False
 
         return True
+
+"""
+Solution 2:
+
+Use Space to reduce time complexity
+Use map for each row, col, box to track used numbers, this can reduce time complexity for 
+isValid function to O(1)
+
+Time Complexity: O()
+Space complexity : O()
+"""
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        self.rows = [defaultdict(int) for _ in range(9)]
+        self.cols = [defaultdict(int) for _ in range(9)]
+        self.boxes = [defaultdict(int) for _ in range(9)]
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                boxIndex = (r // 3) * 3 + c // 3
+                self.putNumber(board, r, c, boxIndex, int(board[r][c]))
+
+        return self.search(board, 0, 0)
+    
+    def search(self, board, r, c):
+        if r == len(board):
+            return True
+        
+        if c == len(board[r]):
+            return self.search(board, r + 1, 0)
+        
+        if board[r][c] != ".":
+            return self.search(board, r, c + 1)
+        else:
+            boxIndex = (r // 3) * 3 + c // 3
+            for candidate in range(1, 10):
+                if not self.isValid(board, r, c, boxIndex, candidate):
+                    continue
+                self.putNumber(board, r, c, boxIndex, candidate)
+                if self.search(board, r, c + 1):
+                    return True
+                self.removeNumber(board, r, c, boxIndex, candidate)
+                board[r][c] = "."
+
+        return False
+    
+    def putNumber(self, board, r, c, boxIndex, candidate):
+        self.rows[r][candidate] = 1
+        self.cols[c][candidate] = 1
+        self.boxes[boxIndex][candidate] = 1
+        board[r][c] = str(candidate)
+
+    def removeNumber(self, board, r, c, boxIndex, candidate):
+        self.rows[r].pop(candidate)
+        self.cols[c].pop(candidate)
+        self.boxes[boxIndex].pop(candidate)
+        board[r][c] = '.'
+
+    def isValid(self, board, row, col, boxIndex, candidate):
+        return not (candidate in self.rows[row] 
+            or candidate in self.cols[col]
+            or candidate in self.boxes[boxIndex])
