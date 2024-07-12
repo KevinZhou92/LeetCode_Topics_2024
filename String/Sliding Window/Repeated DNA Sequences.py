8f33d250edbba9d2ca5f74b51859d9379120a3c2
Pengcheng Zhou <monsterzpc@gmail.com>
2024-07-11 23:54:43 -0700

    Rabin Karp Solution

--- String/Sliding Window/Repeated DNA Sequences.py
+++ String/Sliding Window/Repeated DNA Sequences.py
@@ -6,7 +6,7 @@ we will add it to the resulting list.
 
 
 Time Complexity: O((N - 10) * 10)
-Space complexity : O(N)
+Space complexity : O((N - 10) * 10)
 """
 
 
@@ -27,3 +27,37 @@ class Solution:
             l += 1
 
         return res
+
+"""
+Solution 1:
+
+Rabin-Karp(rolling hash algorithm)
+
+Time Complexity: O(N)
+Space complexity : O(N - L)
+"""
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        if not s:
+            return []
+
+        base = 31
+        mod = float('inf')
+        hashValue = 0
+        seen = {}
+        res = set()
+        right = 0
+        while right < len(s):
+            if right <= 9:
+                hashValue = (hashValue * base + ord(s[right])) % mod
+            else:
+                hashValue = ((hashValue - 31 ** 9 * ord(s[right - 10])) * base + ord(s[right])) % mod
+            if hashValue not in seen:
+                seen[hashValue] = [right - 9]
+            else:
+                res.add(s[right - 9:right + 1])
+            right += 1
+
+        return res
+
+        
